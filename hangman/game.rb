5ebc1586675a11c2311f4c49ff8dffce8caa967e
landermo <lmontgomery@boomtownroi.com>
2017-01-16 10:47:53 -0500

    Refactor

--- hangman/game.rb
+++ hangman/game.rb
@@ -1,75 +1,91 @@
+Dictionary = ["banana", "jazz", "something"]
+
 class Game
   attr_reader :turns_left
-  attr_accessor :guesses
 
  def initialize
-   @word = ["c", "a", "t"]
-   @turns_left = 6
-
-
-   #@letters_remaining = @guesses[3]
+   @guesses = []
+   @word = Dictionary.sample
  end
 
-
-  def take_turn(letter)
-    good_guesses = []
-    bad_guesses = []
-    if turns_left != 0
-
-      @word.each do |x|
-      if @word.include? letter
-        good_guesses.push letter
-        return good_guesses
-      else
-        @turns_left -= 1
-        bad_guesses.push letter
-        return '_'
-      end
-    end
-    end
+  def show_guesses
+    puts @guesses.join(',')
   end
 
-  def word
-    @word = word
-  end
-
-  def good_guesses
-    @good_guesses
-  end
-
-  def bad_guesses
-    @bad_guesses
+  def prompt
+    puts "The answer is #{@word}"
+    print 'Player: Guess a letter >'
+    letter = gets.chomp.downcase.strip
+    @guesses.push letter
   end
 
 
 
-
-
-  def lose
-    lose_game = false
-    if @turns_left == 0
-      lose_game = true
-    end
-    lose_game
-  end
-
-  def player_wins
-    if @good_guesses == @word
-      player_wins = true
-    end
-    player_wins
-    end
-
-  def letter_at (position)
-    @word[position]
-  end
-
-
-  def show_board
-    "
-    #{letter_at} 1 #{letter_at} 2 #{letter_at} 3
-      ---------      --------       --------
-    "
-  end
 end
 
+
+
+g = Game.new
+  loop do
+    g.prompt
+    g.show_guesses
+  end
+
+
+
+
+
+
+#   def take_turn(letter)
+#     if turns_left != 0
+#
+#       @word.each do |x|
+#       if @word.include? letter
+#         good_guesses.push letter
+#         return good_guesses
+#       else
+#         @turns_left -= 1
+#         bad_guesses.push letter
+#         return '_'
+#       end
+#     end
+#     end
+#   end
+#
+#   def word
+#     @word = word
+#   end
+#
+#
+#
+#
+#
+#
+#   def lose
+#     lose_game = false
+#     if @turns_left == 0
+#       lose_game = true
+#     end
+#     lose_game
+#   end
+#
+#   def player_wins
+#     if @good_guesses == @word
+#       player_wins = true
+#     end
+#     player_wins
+#     end
+#
+#   def letter_at (position)
+#     @word[position]
+#   end
+#
+#
+#   def show_board
+#     "
+#     #{letter_at} 1 #{letter_at} 2 #{letter_at} 3
+#       ---------      --------       --------
+#     "
+#   end
+# end
+#
